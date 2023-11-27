ede334150b3e9a33fb8470f988600efdac9622d1
Diana Gakuya <deegakuya@gmail.com>
2023-11-27 05:01:49 +0300

    assignment 1

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    while len(fibonacci_list) < length:
+        if len(fibonacci_list) == 0:
+            fibonacci_list.append(0)
+        elif len(fibonacci_list) == 1:
+            fibonacci_list.append(1)
+        else:
+            next_value = fibonacci_list[-1] + fibonacci_list[-2]
+            fibonacci_list.append(next_value)
+    print(fibonacci_list)
